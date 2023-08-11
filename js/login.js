87d8a0823ce64c9115734c93db7954a9e1ae9b06
facundonavas <facundonc98@gmail.com>
2023-08-11 20:19:58 -0300

    updatelogin.js

--- js/login.js
+++ js/login.js
@@ -1,5 +1,7 @@
 const checkbox = document.getElementById('mostrarPass');
-var password = document.getElementById('password');
+const password = document.getElementById('password');
+const email = document.getElementById('email')
+const button = document.getElementById('button')
 
 checkbox.addEventListener('click', () => {
     if (password.getAttribute("type") === "password") {
@@ -7,5 +9,16 @@ checkbox.addEventListener('click', () => {
     } else {
         password.setAttribute("type", "password");
     }
+    console.log(data)
 });
-   
+
+button.addEventListener('click', (evento) => {
+    evento.preventDefault()
+    const data = {
+        email: email.value,
+        password: password.value
+    }
+    console.log(data)
+})
+
+
