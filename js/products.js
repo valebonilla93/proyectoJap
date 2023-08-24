90e10fce6df1e6d11cb45f553da2411d4b223107
ValeBo46 <valebonilla1993@gmail.com>
2023-08-24 17:25:11 -0300

    orden por precio y relevancia

--- js/products.js
+++ js/products.js
@@ -1,8 +1,14 @@
 const catId = localStorage.getItem('catID');
 const DATA_URL = PRODUCTS_URL + catId + EXT_TYPE;
 const container = document.getElementById("container");
+const mayorAMenor = document.getElementById("sortAsc");
+const menorAMayor = document.getElementById("sortDesc");
+const rel = document.getElementById("sortByCount");
+let dataArray = [];
+let categoryName;
 
 function showData(dataArray, categoryName) {
+  container.innerHTML = '';
   const titulo = document.createElement("h1");
   titulo.textContent = "Productos";
   const subtitulo = document.createElement("h3");
@@ -35,7 +41,28 @@ function showData(dataArray, categoryName) {
   container.appendChild(table);
 }
 
+menorAMayor.addEventListener("click", () => {
+  dataArray.sort((a, b) => a.cost - b.cost);
+  showData(dataArray, categoryName); 
+});
+
+mayorAMenor.addEventListener("click", () => {
+  dataArray.sort((b, a) => a.cost - b.cost);
+  showData(dataArray, categoryName); 
+});
+
+rel.addEventListener("click", () => {
+  dataArray.sort((b, a) => a.soldCount - b.soldCount);
+  showData(dataArray, categoryName); 
+});
+
 
 fetch(DATA_URL)
   .then((response) => response.json())
-  .then((data) => showData(data.products, data.catName))
+  .then((data) => {
+      dataArray = data.products;
+      categoryName = data.catName;
+      showData(dataArray, data.catName);
+  });
+
+ 
