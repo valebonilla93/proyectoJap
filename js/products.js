073ed714679aa813368975c589a4ce7815b82592
¨Brunovbf¨ <bvbf20@gmail.com>
2023-09-07 21:59:06 -0300

    cambioVariables

--- js/products.js
+++ js/products.js
@@ -1,115 +1,113 @@
 const catId = localStorage.getItem('catID');
 const DATA_URL = PRODUCTS_URL + catId + EXT_TYPE;
 const container = document.getElementById("container");
-const mayorAMenor = document.getElementById("sortAsc");
-const menorAMayor = document.getElementById("sortDesc");
-const rel = document.getElementById("sortByCount");
-const precioMin = document.getElementById("rangeFilterCountMin");
-const precioMax = document.getElementById("rangeFilterCountMax");
-const botonFiltrar = document.getElementById("rangeFilterCount");
-const botonLimpiar = document.getElementById("clearRangeFilter");
-const buscador = document.getElementById("search");
+const sortAscending = document.getElementById("sortAsc");
+const sortDescending = document.getElementById("sortDesc");
+const sortByCount = document.getElementById("sortByCount");
+const priceMin = document.getElementById("rangeFilterCountMin");
+const priceMax = document.getElementById("rangeFilterCountMax");
+const filterButton = document.getElementById("rangeFilterCount");
+const clearButton = document.getElementById("clearRangeFilter");
+const searchInput = document.getElementById("search");
 let dataArray = [];
 let categoryName;
-let filtrados = [];
+let filteredData = [];
 
-// Función que agrega al html el contenido a mostrar traido desde el JSON
+// Función que agrega el contenido del JSON al HTML para mostrarlo
 function showData(dataArray, categoryName) {
   container.innerHTML = '';
-  // En el caso de no encontrar resultados en la búsqueda se mostrará este mensaje.
-  if (dataArray.length === 0){
-    const resultado = document.createElement("p");
-    resultado.innerHTML = 'No se han encontrado resultados para tu búsqueda. Asegúrate de que todas las palabras estén escritas correctamente.';
-    container.appendChild(resultado);
+  // En caso de no encontrar resultados en la búsqueda, muestra este mensaje.
+  if (dataArray.length === 0) {
+    const result = document.createElement("p");
+    result.innerHTML = 'No se encontraron resultados para tu búsqueda. Asegúrate de que todas las palabras estén escritas correctamente.';
+    container.appendChild(result);
     return;
   }
-  const titulo = document.createElement("h1");
-  titulo.textContent = "Productos";
-  const subtitulo = document.createElement("h3");
-  subtitulo.innerHTML = `Verás aquí todos los productos de la categoría <em>${categoryName}</em>`;
+  const title = document.createElement("h1");
+  title.textContent = "Productos";
+  const subtitle = document.createElement("h3");
+  subtitle.innerHTML = `Aquí verás todos los productos de la categoría <em>${categoryName}</em>.`;
 
-
-  container.appendChild(titulo);
-  container.appendChild(subtitulo);
+  container.appendChild(title);
+  container.appendChild(subtitle);
 
   const table = document.createElement("table");
   const tbody = document.createElement("tbody");
 
   for (const item of dataArray) {
     const row = document.createElement("tr");
-    // Le agregamos el atributo id a cada fila creada por el for en products.html.
+    // Agrega un atributo id a cada fila creada en el bucle en products.html.
     row.setAttribute("id", item.id);
     row.innerHTML = `
-      <td class="imagen"><img src="${item.image}" alt="${item.name}" style="max-width: 300px;"></td>
+      <td class="image"><img src="${item.image}" alt="${item.name}" style="max-width: 300px;"></td>
     
-      <td class="textoFilas">
-        <p class="productoTitulo" >${item.name} - ${item.currency} ${item.cost} </p>
+      <td class="rowText">
+        <p class="productTitle">${item.name} - ${item.currency} ${item.cost} </p>
         <p>${item.description}</p>
       </td>
-      <td class="textoFilas">${item.soldCount} vendidos </td>
-      
-  
+      <td class="rowText">${item.soldCount} vendidos </td>
     `;
     tbody.appendChild(row);
-    // Agregamos un controlador de eventos que actúe cuando se haga click en la fila redirigiéndonos a product-info.
-  row.addEventListener("click", () => {
-  // Guardamos el id de la fila, es decir del producto en el almacenamiento local.
-    const productId = item.id;
-    localStorage.setItem("selectedProductId", productId);
-    window.location.href = "product-info.html";
-  });
-}
+    // Agrega un controlador de eventos que redirige a product-info cuando se hace clic en una fila.
+    row.addEventListener("click", () => {
+      // Almacena el id de la fila, es decir, el producto, en el almacenamiento local.
+      const productId = item.id;
+      localStorage.setItem("selectedProductId", productId);
+      window.location.href = "product-info.html";
+    });
+  }
 
   table.appendChild(tbody);
   container.appendChild(table);
 }
 
-//Función que ordena de forma ascendente los productos según su valor.
-menorAMayor.addEventListener("click", () => {
+// Función que ordena los productos en orden ascendente por su valor.
+sortAscending.addEventListener("click", () => {
   dataArray.sort((a, b) => a.cost - b.cost);
   showData(dataArray, categoryName); 
 });
 
-//Función que ordena de forma descendente los productos según su valor.
-mayorAMenor.addEventListener("click", () => {
+// Función que ordena los productos en orden descendente por su valor.
+sortDescending.addEventListener("click", () => {
   dataArray.sort((b, a) => a.cost - b.cost);
   showData(dataArray, categoryName); 
 });
-//Función que ordena de forma descendente los productos según la cantidad de vendidos.
-rel.addEventListener("click", () => {
+
+// Función que ordena los productos en orden descendente por la cantidad vendida.
+sortByCount.addEventListener("click", () => {
   dataArray.sort((b, a) => a.soldCount - b.soldCount);
   showData(dataArray, categoryName); 
 });
 
-// Función que filtra los productos de acuerdo a precios mínimos y máximos indicados en los campos correspondientes.
-botonFiltrar.addEventListener("click", () => {
-    filtrados = dataArray.filter(item => item.cost >= precioMin.value && item.cost <= precioMax.value);
-    showData(filtrados, categoryName);
-})
+// Función que filtra los productos en función de los precios mínimo y máximo ingresados en los campos correspondientes.
+filterButton.addEventListener("click", () => {
+  filteredData = dataArray.filter(item => item.cost >= priceMin.value && item.cost <= priceMax.value);
+  showData(filteredData, categoryName);
+});
 
-// Función que limpia los filtros aplicados anteriormente.
-botonLimpiar.addEventListener("click", () => {
-  filtrados = [];
-  precioMin.value = "";
-  precioMax.value = "";
+// Función que borra los filtros aplicados anteriormente.
+clearButton.addEventListener("click", () => {
+  filteredData = [];
+  priceMin.value = "";
+  priceMax.value = "";
   showData(dataArray, categoryName);
-})
-// Buscador en tiempo real de productos
-buscador.addEventListener('input', () => {
-  const valorBuscador = buscador.value.toLowerCase();
-  const arrayBuscador = dataArray.filter(item => {
-    const nombre = item.name.toLowerCase();
-    const descripcion = item.description.toLowerCase();
-    return nombre.includes(valorBuscador) || descripcion.includes(valorBuscador);
+});
 
-  })
-  showData(arrayBuscador, categoryName);
-} )
+// Búsqueda de productos en tiempo real
+searchInput.addEventListener('input', () => {
+  const searchValue = searchInput.value.toLowerCase();
+  const searchResults = dataArray.filter(item => {
+    const name = item.name.toLowerCase();
+    const description = item.description.toLowerCase();
+    return name.includes(searchValue) || description.includes(searchValue);
+  });
+  showData(searchResults, categoryName);
+});
 
 fetch(DATA_URL)
   .then((response) => response.json())
   .then((data) => {
-      dataArray = data.products;
-      categoryName = data.catName;
-      showData(dataArray, data.catName);
+    dataArray = data.products;
+    categoryName = data.catName;
+    showData(dataArray, data.catName);
   });
