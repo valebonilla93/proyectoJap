62acf63d4c7fc7f7c4a3c53f2c03a1c57d540653
ValeBo46 <valebonilla1993@gmail.com>
2023-09-08 12:15:09 -0300

    conflictos solucionados

--- js/product-info.js
+++ js/product-info.js
@@ -1,79 +1,71 @@
 const selectedProductId = localStorage.getItem("selectedProductId");
 const url = PRODUCT_INFO_URL + selectedProductId + EXT_TYPE;
 const container = document.getElementById('container');
-const urlComent = PRODUCT_INFO_COMMENTS_URL + selectedProductId + EXT_TYPE;
-const containerComent = document.getElementById('containerComentarios');
+const urlComment = PRODUCT_INFO_COMMENTS_URL + selectedProductId + EXT_TYPE;
+const containerComment = document.getElementById('containerComentarios');
+const commentBtn = document.getElementById("comentBtn");
+const commentInput = document.getElementById("comentario");
+const email = document.getElementById("email");
 let dataArray = [];
-let dataArrayComent = [];
+let dataArrayComment = [];
+
 
 function showDataInfo(dataArray) {
     container.innerHTML = "";
-
     const title = document.createElement('h3');
     title.innerHTML = `${dataArray.name}`;
     container.appendChild(title);
-
     const cost = document.createElement('p');
     cost.innerHTML = `Precio <br> ${dataArray.currency} ${dataArray.cost}`;
     container.appendChild(cost);
-
     const description = document.createElement('p');
     description.innerHTML = `Descripción <br> ${dataArray.description}`;
     container.appendChild(description);
-
     const category = document.createElement('p');
     category.innerHTML = `Categoría <br> ${dataArray.category}`;
     container.appendChild(category);
-
     const sold = document.createElement('p');
     sold.innerHTML = `Cantidad de vendidos <br> ${dataArray.soldCount}`;
     container.appendChild(sold);
 
     const imgContainer = document.createElement('div');
-    imgContainer.classList.add('d-flex');
-    imgContainer.innerHTML = `Imágenes ilustrativas <br>`;
+    imgContainer.innerHTML = `imagenes ilustrativas <br>`;
     container.appendChild(imgContainer);
 
     for (const image of dataArray.images) {
         const img = document.createElement('img');
         img.setAttribute('src', image);
-        img.style.width = '20%';
-    img.style.height = 'auto';
-        img.classList.add('d-inline-block', 'mx-2');
         imgContainer.appendChild(img);
     }
 
     container.appendChild(imgContainer);
 
 }
-
-function showDataComent(dataArrayComent) {
-    containerComent.innerHTML = "";
+function showDataComment(dataArrayComment) {
+    containerComment.innerHTML = '';
     const title = document.createElement('h3');
-    const table = document.createElement("table");
-    const tbody = document.createElement("tbody");
-    title.textContent = "Comentarios";
-    containerComent.appendChild(title);
-    for (const item of dataArrayComent) {
-        const row = document.createElement("tr");
-        const stars = document.createElement("td");
-        for (let i = 1; i <= 5; i++){
-            const star = document.createElement("span");
-            star.classList
-        }
+    const table = document.createElement('table');
+    const tbody = document.createElement('tbody');
+    title.textContent = 'Comentarios';
+    containerComment.appendChild(title);
+
+    for (const item of dataArrayComment) {
+        const row = document.createElement('tr');
+        const starRating = '&#9733;'.repeat(Math.round(item.score));
+        const emptyStars = '&#9734;'.repeat(5 - Math.round(item.score)); 
+
         row.innerHTML = `
             <td>
-        <p>${item.user} - ${item.dateTime} - ${item.score} </p>
-        <p>${item.description}</p>
-      </td>
-    `;
+                <p>${item.user} - ${item.dateTime} - ${starRating}${emptyStars}</p>
+                <p>${item.description}</p>
+            </td>
+        `;
         tbody.appendChild(row);
     }
 
     table.appendChild(tbody);
-    containerComent.appendChild(table);
+    containerComment.appendChild(table);
 }
-
 fetch(url)
     .then((response) => response.json())
     .then((data) => {
@@ -84,12 +76,40 @@ fetch(url)
         console.error('Fetch error:', error);
     });;
 
-    fetch(urlComent)
+fetch(urlComment)
     .then((response) => response.json())
     .then((dataDos) => {
-        dataArrayComent = dataDos;
-        showDataComent(dataArrayComent);
+        dataArrayComment = dataDos;
+        showDataComment(dataArrayComment);
     })
     .catch((error) => {
         console.error('Fetch error:', error);
     });;
+
+// Agregar un evento al botón "Enviar" para manejar el proceso de agregar comentarios.
+commentBtn.addEventListener("click", function () {
+    // Obtener el contenido del comentario y la puntuación del usuario.
+    const commentText = commentInput.value;
+    const puntuacion = document.getElementById("puntuacion").value;
+
+    // Crear un nuevo objeto de comentario con la información proporcionada por el usuario.
+
+    const newComment = {
+        user: "Usuario" //poner email?,
+        ,dateTime: new Date().toLocaleString(),
+        score: puntuacion,
+        description: commentText
+    };
+
+    // Agregar el nuevo comentario al arreglo de comentarios existentes.
+    dataArrayComment.push(newComment);
+
+    // Guardar el arreglo actualizado en el Local Storage.
+    localStorage.setItem("comentarios", JSON.stringify(dataArrayComment));
+
+    // Llamar a la función showDataComent para mostrar los comentarios actualizados en la página.
+    showDataComment(dataArrayComment);
+
+    // Limpiar el campo de comentario después de agregarlo.
+    commentInput.value = "";
+});
