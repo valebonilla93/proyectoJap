bd8d79d0afb8f18d2b20b4e93f4969e2767a5e92
FacundoNavas <141280016+FacundoNavas@users.noreply.github.com>
2023-09-08 17:13:21 -0300

    Actualizar product-info.js

--- js/product-info.js
+++ js/product-info.js
@@ -139,7 +139,7 @@ fetch(urlComment)
     .then((response) => response.json())
     .then((dataDos) => {
         dataArrayCommentApi = dataDos;
-        dataArrayComment = dataArrayCommentApi.concat(dataArrayComentUser);
+        dataArrayComment = dataArrayCommentApi.concat(dataArrayCommentUser);
         showDataComment(dataArrayComment);
         localStorage.setItem("comentarios", JSON.stringify(dataArrayComment));
     })
